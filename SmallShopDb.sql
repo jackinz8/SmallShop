14f8db7077047c26ac1ef74c70d457f0df7675bd
jackinz <jackinz2010@hotmail.de>
2018-05-03 00:25:51 +0200

    Signed-off-by: jackinz <jackinz2010@hotmail.de>

--- SmallShopDb.sql
+++ SmallShopDb.sql
@@ -1,6 +1,29 @@
 ﻿use Nzhtest
 
---------------------------------------------------------------
+--3 Sys Tables--
+CREATE TABLE [dbo].[Sys_OrderStatus](
+	[OrderStatusId] [int] NOT NULL,
+	[OrderStatusName] [nvarchar](8) NOT NULL,
+	CONSTRAINT PK_OrderStatusId  PRIMARY KEY ([OrderStatusId])
+)
+GO
+CREATE TABLE [dbo].[Sys_CustomerGroup](
+	[DiscountGroupId] [int] NOT NULL,
+	[DiscountGroupName] [nvarchar](3) NOT NULL,
+	[DiscountValue] [int],
+	[DiscountPercent] [int],
+	CONSTRAINT PK_DiscountGroupId PRIMARY KEY ([DiscountGroupId])
+)
+GO
+CREATE TABLE [dbo].[Sys_Currency](
+	[CurrencyId] [int] IDENTITY(1,1) NOT NULL,
+	[Currency] [decimal](10, 2) NOT NULL,
+	[FromDate] [datetime] CONSTRAINT DV_FromDate DEFAULT GETDATE() NOT NULL,
+	CONSTRAINT PK_CurrencyId  PRIMARY KEY ([CurrencyId])
+)
+GO
+
+--3 Ref Tables--
 CREATE TABLE [dbo].[Ref_Article](
 	[ArticleId] [int] IDENTITY(1,1) Primary Key NOT NULL,
 	[ArticleName] [nvarchar](50) NOT NULL,
@@ -11,8 +34,8 @@ CREATE TABLE [dbo].[Ref_Article](
 	[IsActive] bit Default(1)
 )
 
----------------------------------------------------------------
 
+--3 Main Tables--
 CREATE TABLE [dbo].[Orders](
 	OrderId			int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	CustomerId		int NOT NULL FOREIGN KEY REFERENCES Ref_Customers(CustomerId),
